511be7752fc8801ed45f3c0d99c3fa33da459c6e
Romulo <romulo.pagnozzi.com>
2026-02-17 20:15:08 -0300

    feat: improved speed of Random copy

--- game/core/custom_copy.py
+++ game/core/custom_copy.py
@@ -125,6 +125,6 @@ def copy_state(state: GameState) -> GameState:
     new_state.game_phase = state.game_phase
     new_state.action_data = _copy_action_data(state.action_data)
     new_state.round_goal_config = state.round_goal_config
-    new_state.rng = random.Random()
+    new_state.rng = random.Random.__new__(random.Random)
     new_state.rng.setstate(state.rng.getstate())
     return new_state
